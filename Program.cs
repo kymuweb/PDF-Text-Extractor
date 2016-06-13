5a23c1d1cbc80b54062ace0da30ef6de1e14dee6
kymuweb <kymuweb@gmail.com>
2016-06-13 10:37:50 +0100

    PDF-Text-Extractor
    A simple example about how to use iTextSharp to extract text from a PDF
    file.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace PDF_Text_Extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pdfPath = "C:\\mypdf.pdf";
+            PdfReader reader = new PdfReader(pdfPath);
+
+            String output = null;
+            for (int i = 1; i <= reader.NumberOfPages; i++)
+                output = output + PdfTextExtractor.GetTextFromPage(reader, i, new SimpleTextExtractionStrategy());
+
+            Console.Write("PDF Content:" + Environment.NewLine);
+            Console.Write(output);
+            Console.Write(Environment.NewLine + "--EOF--");
+        }
+    }
+}
